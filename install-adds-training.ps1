d5ede286cf3b0a1d2e65ccf904163f3a1a2d8fa4
Cédric Obejero <68953041+w0dd3n@users.noreply.github.com>
2024-11-12 16:55:33 +0100

    Global redesign of the script (all-in-one)

--- install-adds-training.ps1
+++ install-adds-training.ps1
@@ -1,75 +1,152 @@
 <#
-    Powershell script to setup and ADDS server for training purpose
+    Full setup of ADDS Role with training data (Users, Computers, etc.)
     Requires D:\ drive to be setup on server
 
-    Date - 2024/11/08
+    Date 2024/11/12
 #>
 
-## CONFIGURATION VARIABLES
-
-$DomainName = "b3car.rns.aftec.fr"
-$DomainPath = "DC=b3car,DC=rns,DC=aftec,DC=fr"
-$DomainNetbiosName = "B3CAR-RNS"
-$SafeModePassword = ConvertTo-SecureString "Azerty!1234" -AsPlainText -Force
-$DomainAdminUsername = "Administrateur"
-$DomainAdminPassword = ConvertTo-SecureString "Azerty!1234" -AsPlainText -Force
-$LocalAdminPassword = ConvertTo-SecureString "Azerty!1234" -AsPlainText -Force
+## AD CONFIGURATION PARAMETERS
+$ServerName = "ADDS-B3CAR-SRV"
+$ServerIPAddress = "10.0.2.254"
+$ServerNetmask = "24"
+$ServerDefaultGateway = "10.0.2.1"
+$ServerDnsPrimary = "8.8.8.8"
+$ServerDnsSecondary = "1.1.1.1"
 $ShareDrive="D:\"
 
-## GLOBAL VARIABLES
+# Domain Parameters
+$DomainNameDNS = "b3car.rns.aftec.fr"
+$DomainNameNetbios = "B3CAR-RNS"
+$SafeModeClearAdminPassword = "Azerty!1234"
+$SafeModeAdministratorPassword = ConvertTo-SecureString $SafeModeClearAdminPassword -AsPlaintext -Force:$true
 
-# Prepare log file to keep track of setup process
-$Date = Get-Date -Format "ddMMyyyy"
-$Hour = Get-Date -Format "HHmm"
+## SCRIPT CONFIGURATION PARAMETERS
+$CurrentDate = Get-Date -Format "ddMMyyyy"
+$ScriptLogfilePath = "C:\ADConfig_$CurrentDate.log"
 
-$Err_Msg_ShareDrive     = "[ ERROR ] Shares Partition doesn't exist"
-$Err_Msg_ADFeature      = "[ ERROR ] AD DS Role installation FAILED"
+$TaskAction = New-ScheduledTaskAction -Execute "Powershell.exe" -Argument "-File $PSCommandPath -Argument Install"
+$TaskTrigger = New-ScheduledTaskTrigger -AtStartup
+$TaskSettings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries
+$TaskPrincipal = New-ScheduledTaskPrincipal -UserId "SYSTEM" -LogonType ServiceAccount
+$TaskInstallName = "INSTALL-ADDS-TASK"
 
-$Inf_Msg_ShareDrive     = "[ INFO ] Shares Partition ready to setup AD DS"
-$Inf_Msg_ADFeature      = "[ INFO ] AD DS Role installation SUCCESS"
+## MAIN SCRIPT
 
-## SCRIPT MAIN SECTION
+param(
+    [Parameter(Mandatory=$true)]
+    [ValidateSet('Setenv','Install','Config')]
+    [string]$Argument
+)
 
-Start-Transcript -Path C:\AD_config_log_$Date`_$Hour.txt `
-                 -Append `
-                 -IncludeInvocationHeader
+Start-Transcript -Path $ScriptLogfilePath -Append -IncludeInvocationHeader
 
-# Validate Shares Partition Drive availability
-if (Test-Path -Path $ShareDrive) {
-    Write-Output $Inf_Msg_ShareDrive
-} else {
-    Write-Output $Err_Msg_ShareDrive
-    Stop-Transcript
-    exit
+switch ($Argument)  {
+    "Setenv" {
+        Set-ServerEnvironment
+    }
+    "Install" {
+        Install-ADDomainControler
+    }
+    "Config" {
+        Set-ADTopology
+    }
+    default {
+        Write-Output "[ ERROR ] Valid argument list : 'Setenv','Install','Config'"
+        Stop-Transcript
+    }
 }
 
-# Setup AD DS role if not installed
-Install-WindowsFeature -name AD-Domain-Services -IncludeManagementTools -IncludeAllSubFeature
-if ((Get-WindowsFeature -Name AD-Domain-Services).Installed -eq $true) {
-    Write-Output $Inf_Msg_ADFeature
-} else {
-    Write-Output $Err_Msg_ADFeature
+## FUNCTION - Setting local server environment
+##
+function Set-ServerEnvironment {
+    Write-Output "Configuring NIC ..."
+    Remove-NetIPAddress -InterfaceIndex (Get-NetIPConfiguration).InterfaceIndex[0] `
+                        -Confirm:$false
+
+    New-NetIPAddress -InterfaceIndex (Get-NetIPConfiguration).InterfaceIndex[0] `
+                     -IPAddress $ServerIPAddress `
+                     -PrefixLength $ServerNetmask `
+                     -DefaultGateway $ServerDefaultGateway
+
+    Set-DnsClientServerAddress -InterfaceIndex (Get-NetIPConfiguration).InterfaceIndex[0] `
+                               -ServerAddresses ($ServerDnsPrimary,$ServerDnsSecondary)
+    Write-Output "Configuring NIC ... DONE"
+
+    Write-Output "Preparing task ton continue setup after reboot ..."
+    $taskAction = New-ScheduledTaskAction -Execute "Powershell.exe" `
+                                          -Argument "-File $PSCommandPath -Argument Install"
+    Register-ScheduledTask -Action $TaskAction `
+                           -Principal $TaskPrincipal `
+                           -Trigger $TaskTrigger `
+                           -Settings $TaskSettings `
+                           -TaskName $TaskInstallName
+    Write-Output "Preparing task ton continue setup after reboot ... DONE"
+
     Stop-Transcript
-    exit
+
+    Rename-Computer -NewName $ServerName -Force -Restart
 }
 
-# Promote server as domain controler
-Install-ADDSForest `
-  -DomainName $DomainName `
-  -CreateDnsDelegation:$false `
-  -DatabasePath "C:\Windows\NTDS" `
-  -DomainMode "7" `
-  -DomainNetbiosName $DomainNetbiosName `
-  -ForestMode "7" `
-  -InstallDns:$true `
-  -LogPath "C:\Windows\NTDS" `
-  -NoRebootOnCompletion:$True `
-  -SysvolPath "C:\Windows\SYSVOL" `
-  -SafeModeAdministratorPassword $SafeModePassword `
-  -Force:$true
+## FUNCTION - Installing Domain Controler and promoting server
+##
+function Install-ADDomainControler {
+    Unregister-ScheduledTask -TaskName $TaskInstallName -Confirm:$false
 
-# End of logging in file
-Stop-Transcript
-Restart-Computer
+    # Roles to be setup
+    $FeatureList = @("AD-Domain-Services","DNS")
 
-## EOF ##
+    foreach ($Feature in $FeatureList) {
+        if (((Get-WindowsFeature -Name $Feature).InstallState) -eq "Available") {
+            Write-Output "[ INFO ] Feature $Feature will be installed now !"
+            try {
+                Add-WindowsFeature -Name $Feature -IncludeManagementTools -IncludeAllSubFeature
+                Write-Output "[ INFO ] $Feature : Installation SUCCESS"
+            } catch {
+                Write-Output "[ ERROR ] $Feature : Installation FAILED"
+                Stop-Transcript
+                exit
+
+            }
+        } else {
+            Write-Output "[ ERROR ] Feature $Feature NOT AVAILABLE"
+            Stop-Transcript
+            exit
+        }
+    }
+
+    Write-Output "[ INFO ] Installing Domain Controler ..."
+    Import-Module ADDSDeployment
+    Install-ADDSForest -DatabasePath 'C:\Windows\NTDS' `
+                       -DomainMode = 'Default' `
+                       -DomainName = $DomainNameDNS `
+                       -DomainNetbiosName = $DomainNameNetbios `
+                       -ForestMode = 'Default' `
+                       -InstallDns = $true `
+                       -LogPath = 'C:\Windows\NTDS' `
+                       -NoRebootOnCompletion = $true `
+                       -SysvolPath = C:\Windows\SYSVOL `
+                       -SafeModeAdministratorPassword = $SafeModeAdministratorPassword `
+                       -Force = $true `
+                       -CreateDnsDelegation = $false
+    Write-Output "[ INFO ] Installing Domain Controler ... DONE"
+
+    Write-Output "Preparing task to continue setup after reboot ..."
+    $taskAction = New-ScheduledTaskAction -Execute "Powershell.exe" `
+                                          -Argument "-File $PSCommandPath -Argument Config"
+    Register-ScheduledTask -Action $TaskAction `
+                           -Principal $TaskPrincipal `
+                           -Trigger $TaskTrigger `
+                           -Settings $TaskSettings `
+                           -TaskName $TaskInstallName
+    Write-Output "Preparing task to continue setup after reboot ... DONE"
+
+    Stop-Transcript
+
+    Restart-Computer
+}
+
+## FUNCTION - Build up AD environment with users, computers and shares
+##
+function Set-ADTopology {
+
+}
